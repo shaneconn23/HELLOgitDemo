26edf702f386fa8f3c4a8bf91c8f3880693ff677
Shane Conn <shaneconn23@gmail.com>
2022-04-27 18:43:14 -0400

    adding css

--- app.js
+++ app.js
@@ -1,5 +1,13 @@
-for (let i = 0; i < 10; i++){
-    console.log(i);
+function randomRGB(){
+    const r = Math.floor(Math.random() * 256);
+    const g = Math.floor(Math.random() * 256);
+    const b = Math.floor(Math.random() * 256);
+    return `rgb(${r}, ${g}, ${b})`
 }
 
-let cucumber = true;
+const letters = document.querySelectorAll('.letter');
+const intervalId = setInterval(function (){
+    for(let letter of letters){
+        letter.style.color = randomRGB();
+    }
+}, 1000);
